20bd419a6b10220c5e05e2565deb9911f8f04917
ersalomo <2019104830@student.kalbis.ac.id>
2023-01-19 15:54:07 +0700

    save me from tears

--- src/Domains/threads/entities/AddThread.js
+++ src/Domains/threads/entities/AddThread.js
@@ -8,9 +8,7 @@ class AddThread {
   }
 
   _verifyPayload({owner, title, body}) {
-    if (!owner || !title || !body) {
-      throw new Error('ADD_THREAD.NOT_CONTAIN_NEEDED_PROPERTY');
-    }
+    if (!owner || !title || !body) throw new Error('ADD_THREAD.NOT_CONTAIN_NEEDED_PROPERTY');
     if (typeof owner !== 'string' || typeof title !== 'string' || typeof body !== 'string') {
       throw new Error('ADD_THREAD.NOT_MEET_DATA_TYPE_SPECIFICATION');
     }
