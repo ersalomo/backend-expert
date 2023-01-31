20a2ab7828eb220c33c6dc8fa8b05c7ca46369b9
ersalomo <ersalomo2002@gmail.com>
2023-01-31 14:36:03 +0700

    add likes table helper

--- src/Applications/use_case/_test/LikeCommentUseCase.test.js
+++ src/Applications/use_case/_test/LikeCommentUseCase.test.js
@@ -1,1 +1,44 @@
-describe('', () => {});
+const LikesRepository = require('../../../Domains/likes/LikesRepository');
+const AddedLike = require('../../../Domains/likes/entities/AddedLike');
+const LikeCommentUseCase = require('../LikeCommentUseCase');
+
+describe('LikeCommentUseCase.test', () => {
+  it('should orchestrating the add thread action correctly', async () => {
+    // Arrange
+    const useCasePayload = {
+      idComment: 'comment-123',
+      owner: 'user-123',
+    };
+
+    /** creating dependency of use case */
+    const mockLikesRepository = new LikesRepository();
+
+    /** mocking needed function */
+    mockLikesRepository.likeComment = jest.fn().mockImplementation(() =>
+      Promise.resolve(
+        new AddedLike({
+          id: 'like-123',
+          id_comment: useCasePayload.idComment,
+          owner: useCasePayload.owner,
+        })
+      )
+    );
+    /** creating use case instance */
+    const getLikeUseCase = new LikeCommentUseCase({
+      likesRepository: '',
+      commentsRepository: '',
+    });
+
+    // Action
+    const addedLike = await getLikeUseCase.execute(useCasePayload);
+
+    // Assert
+    expect(addedLike).toStrictEqual(
+      new AddedLike({
+        id: 'like-123',
+        id_comment: useCasePayload.idComment,
+        owner: useCasePayload.owner,
+      })
+    );
+  });
+});
--- src/Infrastructures/repository/_test/LikeRepositoryPostgres.test.js
+++ src/Infrastructures/repository/_test/LikeRepositoryPostgres.test.js
@@ -1,3 +1,28 @@
+const ThreadTableTestHelper = require('../../../../tests/ThreadTableTestHelper');
+const UsersTableTestHelper = require('../../../../tests/UsersTableTestHelper');
+const CommentTableTestHelper = require('../../../../tests/CommentTableTestHelper');
+const pool = require('../../database/postgres/pool');
+const LikesTableTestHelper = require('../../../../tests/LikesTableTestHelper');
+const AddLike = require('../../../Domains/likes/entities/AddLike');
+const AddedLike = require('../../../Domains/likes/entities/AddedLike');
+const LikeRepositoryPostgres = require('../../repository/LikeRepositoryPostgres');
+
 describe('LikeRepositoryPostgres', () => {
-  it('', async () => {});
+  beforeAll(async () => {
+    await UsersTableTestHelper.addUser({});
+  });
+
+  afterEach(async () => {
+    await ThreadTableTestHelper.cleanTable();
+    await CommentTableTestHelper.cleanTable();
+    // await ReplayTableTestHelper.cleanTable();
+  });
+
+  afterAll(async () => {
+    await UsersTableTestHelper.cleanTable();
+    await pool.end();
+  });
+  describe('', async () => {
+    it('', async () => {});
+  });
 });
